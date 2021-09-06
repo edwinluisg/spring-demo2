213153d16eb3639e861d427189d7b48a207aa3c4
Edwin luis <luis.edwin06@gmail.com>
2021-09-06 14:10:03 -0400

    Atributes entity user jpa

--- src/main/java/com/luis/demo3/entity/User.java
+++ src/main/java/com/luis/demo3/entity/User.java
@@ -2,14 +2,17 @@ package com.luis.demo3.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name", length = 300, nullable = false)
     private String name;
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 }
